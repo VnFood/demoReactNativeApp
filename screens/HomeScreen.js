7115400542096de1610bf8a585a91b3b61bb4118
vnfood <tranloi.tphl@gmail.com>
2020-05-08 21:32:41 +0700

    LoiTPH - Add login, register screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as firebase from "firebase";
 
 export default class HomeScreen extends React.Component {
+  state = {
+    email: "",
+    displayName: "",
+  };
+
+  componentDidMount() {
+    const { email, displayName } = firebase.auth().currentUser;
+    this.setState({ email, displayName });
+  }
+
+  signOutUser = () => {
+    firebase.auth().signOut();
+  };
+
   render() {
     return (
       <View style={stylesHomeScreen.container}>
-        <Text>Home Screen</Text>
+        <Text>Hi {this.state.email}!</Text>
+
+        <TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -15,6 +34,6 @@ const stylesHomeScreen = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
-  }
-})
+    alignItems: "center",
+  },
+});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,83 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import * as firebase from "firebase";
 
 export default class LoginScreen extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    errorMessage: null,
+  };
+
+  handleLogin = () => {
+    const { email, password } = this.state;
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => this.setState({ errorMessage: error.message }));
+  };
+
   render() {
     return (
       <View style={stylesLoginScreen.container}>
-        <Text>Login Screen</Text>
+        <Text style={stylesLoginScreen.greeting}>
+          {`Hello again.\nWelcome back.`}
+        </Text>
+
+        <View style={stylesLoginScreen.errorMessage}>
+          {this.state.errorMessage && (
+            <Text style={stylesLoginScreen.error}>
+              {this.state.errorMessage}
+            </Text>
+          )}
+        </View>
+
+        <View style={stylesLoginScreen.form}>
+          <View>
+            <Text style={stylesLoginScreen.inputTitle}>Email Address</Text>
+            <TextInput
+              style={stylesLoginScreen.input}
+              autoCapitalize="none"
+              onChangeText={(email) => this.setState({ email })}
+              value={this.state.email}
+            ></TextInput>
+          </View>
+
+          <View style={{ marginTop: 32 }}>
+            <Text style={stylesLoginScreen.inputTitle}>Password</Text>
+            <TextInput
+              style={stylesLoginScreen.input}
+              secureTextEntry
+              autoCapitalize="none"
+              onChangeText={(password) => this.setState({ password })}
+              value={this.state.password}
+            ></TextInput>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={stylesLoginScreen.button}
+          onPress={this.handleLogin}
+        >
+          <Text style={{ color: "#FFF", fontWeight: "500" }}>Sign in</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{ alignSelf: "center", marginTop: 32 }}
+          onPress={() => this.props.navigation.navigate("Register")}
+        >
+          <Text style={{ color: "#414959", fontSize: 13 }}>
+            New to AwesomeProject?&nbsp;
+            <Text style={{ fontWeight: "500", color: "#E9446A" }}>Sign Up</Text>
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -14,7 +86,47 @@ export default class LoginScreen extends React.Component {
 const stylesLoginScreen = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  greeting: {
+    marginTop: 32,
+    fontSize: 18,
+    fontWeight: "400",
+    textAlign: "center",
+  },
+  errorMessage: {
+    height: 72,
+    alignItems: "center",
     justifyContent: "center",
-    alignItems: "center"
-  }
-})
+    marginHorizontal: 30,
+  },
+  error: {
+    color: "#E9446A",
+    fontSize: 13,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  form: {
+    marginBottom: 48,
+    marginHorizontal: 30,
+  },
+  inputTitle: {
+    color: "#8A8F9E",
+    fontSize: 10,
+    textTransform: "uppercase",
+  },
+  input: {
+    borderBottomColor: "#8A8F9E",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    height: 40,
+    fontSize: 15,
+    color: "#161F3D",
+  },
+  button: {
+    marginHorizontal: 30,
+    backgroundColor: "#E9446A",
+    borderRadius: 4,
+    height: 52,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
